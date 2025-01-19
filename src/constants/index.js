d7381e4acb195d2a580738fdd78e864c708ab6fb
Andrii <m9ta92@gmail.com>
2025-01-19 18:39:02 +0100

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,7 @@ export const SMTP = {
 
 import path from 'node:path';
 
-export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src/templates');
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
