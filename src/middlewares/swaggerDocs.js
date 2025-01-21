449b27c97f291e792b3ee3180de746b7f03b1128
Andrii <m9ta92@gmail.com>
2025-01-21 22:09:53 +0100

    fix_1

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -3,14 +3,11 @@ import swaggerUI from 'swagger-ui-express';
 import fs from 'node:fs';
 
 import { SWAGGER_PATH } from '../constants/index.js';
-import path from 'node:path';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = JSON.parse(
-      fs.readFileSync(path.resolve(SWAGGER_PATH), 'utf-8'),
-    );
-    return [swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf-8'));
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch (err) {
     return (req, res, next) =>
       next(createHttpError(500, "Can't load swagger docs"));
