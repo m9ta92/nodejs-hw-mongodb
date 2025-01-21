b61ffe198ee5e8cce526e69b4c6148d46a125a5f
Andrii <m9ta92@gmail.com>
2025-01-21 21:32:27 +0100

    fix_1

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,4 +19,4 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
 };
 
-export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+export const SWAGGER_PATH = path.resolve(process.cwd(), 'docs', 'swagger.json');
